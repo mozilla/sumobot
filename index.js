c3aab4be31a7726cac3a4cc334034f69a1d6882e
SamuelMoraesF <samuel@samuelmoraes.com>
2015-07-24 17:59:24 -0300

    Config file and command support

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-var irc = require('irc');
-var client = new irc.Client('irc.mozilla.org', 'botnick', {
-    channels: ['#sumobotdev'],
-});
+var irc = require('irc'),
+    config = require('./config');
 
+var options = {
+        channels: config.irc.channels,
+        autoRejoin: true,
+        port: config.irc.port,
+        secure: config.irc.ssl
+    };
+
+var client = new irc.Client(config.irc.server, config.irc.nick, {channels: config.irc.channels});
+
+client.addListener('message', function(from, to, message) {
+
+    if (message.search('[!:]command') >= 0) {
+        client.say(to, "reply");
+    }
+
+});
