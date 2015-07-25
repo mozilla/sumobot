092b216eab74c8b395fee8ba769f05dfb284c8a3
SamuelMoraesF <samuel@samuelmoraes.com>
2015-07-25 04:54:41 -0300

    Welcome message

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var fs = require('fs'),
     irc = require('irc'),
-    config = require('./config');
+    sqlite3 = require('sqlite3'),
+    config = require('./config'),
+    db = new sqlite3.Database(config.bot.database);
 
 var options = {
         channels: config.irc.channels,
@@ -9,9 +11,16 @@ var options = {
         secure: config.irc.ssl
     };
 
+// Setup database if not exists
+if (!fs.existsSync(config.bot.database)) {
+    db.run("CREATE TABLE notified (id INTEGER PRIMARY KEY AUTOINCREMENT, nick TEXT UNIQUE)");
+}
+
+// Connect to IRC Server
 var client = new irc.Client(config.irc.server, config.irc.nick, options);
 
-function log(message){
+// Logs function
+function log(message) {
 
     if (config.logs.stdout) {
         console.log(message)
@@ -25,10 +34,60 @@ function log(message){
 
 };
 
+// Add nick to notifieds table function
+function addNick(nick) {
+
+    db.run("INSERT INTO notified (id, nick) VALUES (NULL, '" + nick + "')");
+
+}
+
+// Messages
 client.addListener('message', function(from, to, message) {
 
     if (message.search('[!:]command') >= 0) {
         client.say(to, "reply");
     }
+
+});
+
+// Join
+client.addListener('join', function(channel, nick, message) {
+
+    if (nick != config.irc.nick) {
+
+        db.get("SELECT id FROM notified WHERE notified.nick = '" + nick + "'", function (err, row) {
+
+            if (row == undefined) {
+
+                client.say(nick, config.bot.welcomemessage);
+                addNick(nick);
+
+            }
+
+        });
+
+    }
+
+});
+
+// Nick change
+client.addListener('nick', function(oldnick, newnick, channels, message) {
+
+    db.get("SELECT id FROM notified WHERE notified.nick = '" + newnick + "'", function (err, row) {
+
+        if (row == undefined) {
+
+            addNick(newnick);
+
+        }
+
+    });
+
+});
+
+// Errors
+client.addListener('error', function(message) {
+
+    console.log('Error: ', message);
 
 });
